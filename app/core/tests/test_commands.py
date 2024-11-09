8d39109f96e6377a96f6ee927d406ca1870fa5fc
Zhiting Lu <zhitinglu@outlook.com>
2024-11-09 13:17:33 +0100

    wrote tests for wait_for_db_command

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,43 @@
+"""
+Test custom django management commands
+"""
+
+from unittest.mock import patch
+
+from psycopg import OperationalError as PsycopgError
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready"""
+        patched_check.return_value = True  # Mocking the behavior of `check`
+        
+        # Call the command to check the database status
+        call_command('wait_for_db')
+        
+        # Assert that `check` was called once with the expected arguments
+        patched_check.assert_called_once_with(database=['default'])
+     
+    @patch('time.sleep')   
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError"""
+        # create a list with two instances of PsycopgError."
+        # So the first two times the mocked check() method is called, it will raise a PsycopgError.
+        patched_check.side_effect = [PsycopgError] * 2 + \
+            [OperationalError] * 3 + [True]
+            # create a list with three instances of OperationalError." 
+            # So the next three times the mocked check() method is called, it will raise an OperationalError
+            # So after raising OperationalError three times, 
+            # the next call will return True (indicating that the database is ready).
+            
+        call_command("wait_for_db")
+        
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(database=['default'])
