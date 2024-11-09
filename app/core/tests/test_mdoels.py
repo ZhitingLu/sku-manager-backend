8e49675fd4aa0f82d624f44f58a12d06683936bc
Zhiting Lu <zhitinglu@outlook.com>
2024-11-09 18:47:41 +0100

    added tests for creating a new user with no email raises error

--- app/core/tests/test_mdoels.py
+++ app/core/tests/test_mdoels.py
@@ -32,3 +32,8 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email(self):
+        """Test creating a new user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
