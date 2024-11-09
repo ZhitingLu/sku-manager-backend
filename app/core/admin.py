8e25920ffb236d913ac7313e4ca9b03db4314aee
Zhiting Lu <zhitinglu@outlook.com>
2024-11-09 20:34:27 +0100

    Listed users on the Django admin interface

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+"""
+Django admin customizaiton
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Sets how the user model is listed on Django's admin interface.'
+    """
+    ordering = ['id']
+    list_display = ['id', 'name', 'email', 'is_active', 'is_staff', 'is_superuser']
+    serch_fields = ['id', 'name', 'email']
+
+
+admin.site.register(models.User, UserAdmin)
