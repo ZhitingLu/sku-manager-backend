2bcbed62205e90241499040394791390977a00ba
Zhiting Lu <zhitinglu@outlook.com>
2024-11-10 21:04:17 +0100

    medication_sku - Views: implemented bulk create API

--- app/medication_sku/views.py
+++ app/medication_sku/views.py
@@ -1,9 +1,11 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from core.models import MedicationSKU
 from medication_sku import serializers
@@ -27,3 +29,22 @@ class MedicationSKUViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new medication sku"""
         serializer.save(user=self.request.user)
+
+    @action(detail=False, methods=['post'], url_path='bulk_create')
+    def bulk_create(self, request):
+        """Bulk create medication SKUs"""
+        # Extract the list of medication SKUs from the request data
+        data = request.data
+
+        serializer = serializers.MedicationSKUSerializer(data=data, many=True)
+        if serializer.is_valid():
+            # Bulk create the medication SKUs
+            medication_skus = []
+            for item in serializer.validated_data:
+                item['user'] = request.user  # Set the user to each SKU
+                medication_skus.append(MedicationSKU(**item))
+
+            MedicationSKU.objects.bulk_create(medication_skus)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
