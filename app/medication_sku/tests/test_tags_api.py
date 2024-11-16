fcdb480d472ac070469221a2f510e142a3a9047b
Zhiting Lu <zhitinglu@outlook.com>
2024-11-16 07:07:14 +0100

    medication_sku - Tests: wrote test for update tag API

--- app/medication_sku/tests/test_tags_api.py
+++ app/medication_sku/tests/test_tags_api.py
@@ -15,6 +15,11 @@ from medication_sku.serializers import TagSerializer
 TAGS_URL = reverse('medication_sku:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail URL."""
+    return reverse('medication_sku:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create a test user"""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -53,3 +58,15 @@ class PrivateTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_update_tag(self):
+        """Test updating tag"""
+        tag = Tag.objects.create(user=self.user, name='Antibiotic')
+        payload = {'name': 'Antiviral'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()  # explicitly refresh the db
+
+        self.assertEqual(tag.name, payload['name'])
